62dfcc16429b5a74bcc21478103d2e153b77d90d
Your Name <erickbennett@Ericks-MacBook-Pro.local>
2015-01-19 18:20:18 -0800

    Initial Commit

---
+++ MapExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapExample
+//
+//  Created by Erick Bennett on 1/19/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapExample
+//
+//  Created by Erick Bennett on 1/19/15.
+//  Copyright (c) 2015 Erick Bennett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
